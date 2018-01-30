23b29e05bb46f5f53592567610703dcacdafebf4
ScriptakiasGiaPaixnidia <35961072+ScriptakiasGiaPaixnidia@users.noreply.github.com>
2018-01-30 19:53:06 +0200

    sc

---
+++ Virus.py
@@ -0,0 +1,16 @@
+import webbrowser
+
+
+print "                             CODED BY SCRIPTFORGAMES"
+
+def VIRUSALERT():
+    "enas aplos ios gramenos se python"
+
+print VIRUSALERT()
+
+trexo = True
+
+site = raw_input("Bale edw to site: ")
+while trexo:
+    print webbrowser.open_new_tab(site)
+
